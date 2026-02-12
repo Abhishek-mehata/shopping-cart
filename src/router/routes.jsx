4773185ad5ceb23b77922477234ddbfeb2aa6630
Abhishek-mehata <mehtaabishekkumar99gmail.com>
2026-02-12 22:07:47 +0545

    solving routes problem

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,29 +1,48 @@
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 
-// Components and pages imports would go here
-import App from "../App.jsx"
-import HomePage from "../pages/HomePage.jsx";
-import ProductsPage from "../pages/ProductsPage.jsx";
-import CartPage from "../pages/CartPage.jsx";
-import Checkout from "../pages/Checkout.jsx";
+// // Components and pages imports would go here
+// import App from "../App.jsx"
+// import HomePage from "../pages/HomePage.jsx";
+// import ProductsPage from "../pages/ProductsPage.jsx";
+// import CartPage from "../pages/CartPage.jsx";
+// import Checkout from "../pages/Checkout.jsx";
 
 
 
-const routes = createBrowserRouter([
+// const routes = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App />,
+//         errorElement: <div>404 this route could not be found! :</div>,
+//         children: [
+//             // Define child routes here
+//             { index: true, element: <HomePage /> },
+//             { path: "products", element: <ProductsPage /> },
+//             { path: "cart", element: <CartPage /> },
+//             { path: "checkout", element: <Checkout /> },
+//         ]
+
+//     }
+// ]);
+
+
+// export default routes;
+
+
+import { createHashRouter } from "react-router-dom";
+
+const routes = createHashRouter([
     {
         path: "/",
         element: <App />,
         errorElement: <div>404 this route could not be found! :</div>,
         children: [
-            // Define child routes here
             { index: true, element: <HomePage /> },
             { path: "products", element: <ProductsPage /> },
             { path: "cart", element: <CartPage /> },
             { path: "checkout", element: <Checkout /> },
-        ]
-
-    }
+        ],
+    },
 ]);
-
 
 export default routes;
